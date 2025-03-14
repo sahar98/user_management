ae03fcd0f0fef3899802b10c947a5e9d9bf49146
NAM <ahmadinaghedi.sahar@gmail.com>
2025-03-14 05:56:14 +0330

    first commit it is a small project for testing other project

---
+++ src/main/java/com/sahar/user_management/Person.java
@@ -0,0 +1,12 @@
+package com.sahar.user_management;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+
+@Data
+public class Person {
+    private Long id;
+    private String firstName;
+    private String lastName;
+}
